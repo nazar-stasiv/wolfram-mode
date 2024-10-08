111f1cdea98a8a556598560ff79030c9a3ba70fc
Nazar Stasiv <nazar@autistici.org>
2024-10-08 08:29:45 +0300

    misc

---
+++ wolfram-mode-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "wolfram-mode" "20180307.13"
+  "Mathematica editing and inferior mode."
+  '((emacs "24.3"))
+  :url "https://github.com/kawabata/wolfram-mode"
+  :commit "be680190cac6ccf579dbce107deaae495928d1b3"
+  :revdesc "be680190cac6"
+  :keywords '("languages" "processes" "tools")
+  :authors '(("Daichi Mochihashi" . "daichiatcslab.kecl.ntt.co.jp"))
+  :maintainers '(("Daichi Mochihashi" . "daichiatcslab.kecl.ntt.co.jp")))
